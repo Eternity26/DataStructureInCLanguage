c6ff2029e5e2c042f5157172b7a560acb049708f
Eternity26 <2118252881@qq.com>
2021-09-08 21:01:48 +0800

    update Tree and Graph

---
+++ cmake-build-debug/CMakeFiles/GraphNote.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GraphNote.dir/Graph/GraphNote.cpp.obj"
+  "GraphNote.exe"
+  "GraphNote.exe.manifest"
+  "GraphNote.pdb"
+  "libGraphNote.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GraphNote.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
